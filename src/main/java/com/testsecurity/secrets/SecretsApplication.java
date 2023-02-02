9b743f34570365ba11151e26a02c0e3bca44efee
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-02-03 03:09:47 +0500

    a

---
+++ src/main/java/com/testsecurity/secrets/SecretsApplication.java
@@ -0,0 +1,12 @@
+package com.testsecurity.secrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SecretsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testsecurity/secrets/SecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testsecurity.secrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
